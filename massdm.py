34864b61b861d1facea7665b8637ea675bae92e0
$Y <58207832+WeLoveuSyrus@users.noreply.github.com>
2021-05-15 04:00:32 -0700

    Update massdm.py

--- massdm.py
+++ massdm.py
@@ -1,48 +1,37 @@
 import discord
 import requests
 from colorama import Fore
-import threading
-import time
-from colored import fg
-
-
-def SendMessage(recipient_id, user, session: requests.Session, content):
-  res = session.post(f"https://discord.com/api/v8/channels/{recipient_id}/messages", json={ 'content': f'{user.mention} '+content}, headers={ "Authorization": "Bot "+TOKEN})
-  if res.status_code == 204 or 200 or 201:
-    print(f"Sent message to {user.name}")
-  elif res.status_code == 429:
-    time.sleep(res.json()['retry_after'])
-    SendMessage(recipient_id, user, session, content)
- 
-class MassDm(discord.Client):
-  def __init__(self, *args, **kwargs):
-    super().__init__(*args, **kwargs)
-    self.color = fg('#ff4b00')
-    self.session = requests.Session()
-    self.ansi = """
-                                             █████╗ ██████╗  ██████╗  ██████╗ 
-                                            ██╔══██╗╚════██╗██╔═████╗██╔═████╗
-                                            ╚█████╔╝ █████╔╝██║██╔██║██║██╔██║
-                                            ██╔══██╗ ╚═══██╗████╔╝██║████╔╝██║
-                                            ╚█████╔╝██████╔╝╚██████╔╝╚██████╔╝
-                                             ╚════╝ ╚═════╝  ╚═════╝  ╚═════╝ 
-                                       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━
+from colored import fg, attr
+class colors:
+    red = fg('#ff004d8')
+    reset = attr('reset')
+    gray = fg('#ff4b00')
+    gray = fg('#ff4b00')
+TOKEN = "ODMyMjI0MDU4ODM3ODI3NTg0.YHgrDQ.fDGDj9oFnkbLyMeemArMYH1ANdA"
+client = discord.Client(intents=discord.Intents.all())
+print(f'''
+                                             {Fore.LIGHTBLACK_EX}█████{colors.gray}╗ {Fore.LIGHTBLACK_EX}██████{colors.gray}╗  {Fore.LIGHTBLACK_EX}██████{colors.gray}╗  {Fore.LIGHTBLACK_EX}██████{colors.gray}╗ 
+                                            {Fore.LIGHTBLACK_EX}██{colors.gray}╔══{Fore.LIGHTBLACK_EX}██{colors.gray}╗╚════{Fore.LIGHTBLACK_EX}██{colors.gray}╗{Fore.LIGHTBLACK_EX}██{colors.gray}╔═{Fore.LIGHTBLACK_EX}████{colors.gray}╗{Fore.LIGHTBLACK_EX}██{colors.gray}╔═{Fore.LIGHTBLACK_EX}████{colors.gray}╗
+                                            {Fore.LIGHTBLACK_EX}{colors.gray}╚{Fore.LIGHTBLACK_EX}█████{colors.gray}╔╝{Fore.LIGHTBLACK_EX} █████{colors.gray}╔╝{Fore.LIGHTBLACK_EX}██{colors.gray}║{Fore.LIGHTBLACK_EX}██{colors.gray}╔{Fore.LIGHTBLACK_EX}██{colors.gray}║{Fore.LIGHTBLACK_EX}██{colors.gray}║{Fore.LIGHTBLACK_EX}██{colors.gray}╔{Fore.LIGHTBLACK_EX}██{colors.gray}║
+                                            {Fore.LIGHTBLACK_EX}{Fore.LIGHTBLACK_EX}██{colors.gray}╔══{Fore.LIGHTBLACK_EX}██{colors.gray}╗ ╚═══{Fore.LIGHTBLACK_EX}██{colors.gray}╗{Fore.LIGHTBLACK_EX}████{colors.gray}╔╝{Fore.LIGHTBLACK_EX}██{colors.gray}║{Fore.LIGHTBLACK_EX}████{colors.gray}╔╝{Fore.LIGHTBLACK_EX}██{colors.gray}║
+                                            {Fore.LIGHTBLACK_EX}{colors.gray}╚{Fore.LIGHTBLACK_EX}█████{colors.gray}╔╝{Fore.LIGHTBLACK_EX}██████{colors.gray}╔╝╚{Fore.LIGHTBLACK_EX}██████{colors.gray}╔╝╚{Fore.LIGHTBLACK_EX}██████{colors.gray}╔╝
+                                             {colors.gray}╚════╝ ╚═════╝  ╚═════╝  ╚═════╝ 
+                                       ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━ 
                                          ━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━  
-                                        """.replace("█", f"{Fore.LIGHTBLACK_EX}█{self.color}")
-
-  async def on_connect(self):
-    print(self.ansi)
-    guild_id = int(input("Server ID: "))
-    message = str(input("Mass Dm Message: "))
-    guild = self.get_guild(guild_id)
-    for member in guild.members:
+''')
+@client.event
+async def on_connect():
+   ask = int(input("server id: "))
+   msg = input("what u wanna say?: ")
+   guild = client.get_guild(ask)
+   session = requests.Session()
+   for member in guild.members:
       try:
         data = await member.create_dm()
-        threading.Thread(target=SendMessage, args=(data.id, data.recipient, self.session, message,)).start()
+        r = session.post(f"https://discord.com/api/v8/channels/{data.id}/messages", json={'content': f'{data.recipient.mention} '+msg}, headers={'Authorization': f'Bot {TOKEN}'})
+        if r.status_code == 204 or 201 or 200:
+           print(f"> DMED {data.recipient.name}")
       except:
         continue
 
-
-client = MassDm(intents=discord.Intents.all())
-TOKEN = open('Token.txt', 'r').readline()
 client.run(TOKEN)
